2d2d854fbf45a59d83340f8665a22007ad76d957
danielcovill <daniel.covill@gmail.com>
2026-01-29 04:23:05 -0700

    Update darkhn.user.js

--- darkhn.user.js
+++ darkhn.user.js
@@ -2,6 +2,6 @@
 // @name Dark HN
 // @match https://news.ycombinator.com/*
 // ==/UserScript==
-window.document.body.style.background = "#222"
-window.document.body.style.color = "#eee"
-window.document.getElementsByTagName("a").forEach(e=>e.setAttribute('style','color:#eee'))
+window.document.body.style.background = '#222'
+window.document.body.style.color = '#eee'
+[...window.document.getElementsByTagName('a')].forEach(e=>e.setAttribute('style','color:#eee'))
