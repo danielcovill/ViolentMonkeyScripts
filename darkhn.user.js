9ee8d718941bfcdaa2c1c6ee81f4f120cd84d277
danielcovill <daniel.covill@gmail.com>
2026-01-29 13:01:11 -0700

    Update darkhn.user.js

--- darkhn.user.js
+++ darkhn.user.js
@@ -2,6 +2,7 @@
 // @name Dark HN
 // @match https://news.ycombinator.com/*
 // ==/UserScript==
-window.document.body.style.background = '#111'
-window.document.body.style.filter = 'saturate(0) invert(1) hue-rotate(180deg)'
+window.document.body.style.background = '#EEE'
+[...(window.document.getElementsByTagName('a'))].forEach(e => e.style.color="#444")
+window.document.getElementById("hnmain").getElementsByTagName("td")[0].style.backgroundColor = "#CCC"
 window.document.getElementsByTagName('img')[0].style.filter = 'saturate(0)'
